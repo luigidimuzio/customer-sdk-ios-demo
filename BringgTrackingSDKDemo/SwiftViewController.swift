566900443ed422452237d7a6be978b83858eb6dc
Luigi Di Muzio <dimuz@MacBook-Pro-di-Luigi.local>
2016-10-05 06:21:31 -0700

    swift compiler complains about 'non-modular header' in framework even with flag set to YES

---
+++ BringgTrackingSDKDemo/SwiftViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SwiftViewController.swift
+//  BringgTrackingSDKDemo
+//
+//  Created by Luigi Di Muzio on 05/10/16.
+//  Copyright © 2016 Matan Poreh. All rights reserved.
+//
+
+import UIKit
+import BringgTracking
+
+class SwiftViewController: UIViewController {
+    
+}
